83f1bd85f31b36269010857104ce5bfcaa0e04a5
Bappaditya <721135bappa@gmail.com>
2022-12-19 11:47:41 +0530

    finished project

--- src/data/data.js
+++ src/data/data.js
@@ -10,6 +10,7 @@ export default [
         img: mountfuji,
         date: "12 JAN, 2021 - 24 JAN, 2021",
         discription: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        link: "https://www.google.com/maps/place/Mount+Fuji/@35.3612022,138.7246275,16z/data=!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634",
     },
 
     {
@@ -19,6 +20,7 @@ export default [
         img: operahouse,
         date: "27 MAY, 2021 - 08 JUN, 2021",
         discription: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+        link: "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8568388,151.2153291,19.75z/data=!3m1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m5!3m4!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967",
     },
 
     {
@@ -28,5 +30,6 @@ export default [
         img: geirangerfjord,
         date: "01 OCT, 2021 - 18 NOV, 2021",
         discription: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        link: "https://www.google.com/maps/place/Geirangerfjord/@62.1048309,7.0050026,12z/data=!3m1!4b1!4m5!3m4!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817",
     },
 ]
